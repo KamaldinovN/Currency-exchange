f360e7b46b85e06803ea491b0bd0532d0b19f1d2
KamaldinovN <n.kamaldinov@gmail.com>
2022-08-28 00:08:27 +0300

    fix margin

--- src/Components/header/header.jsx
+++ src/Components/header/header.jsx
@@ -1,36 +1,33 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
 // eslint-disable-next-line
-import style from "./style.css"
+import style from "./style.css";
 
-
-
-export default function Header({date, USD, EUR, GBP}) {
-
-    return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar className='color' position="static">
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                        Currency Converter
-                    </Typography>
-                    <Typography className="todayCurrency"  variant="h7" component="div">
-                        USD: {USD}$
-                    </Typography>
-                    <Typography  className="todayCurrency" variant="h7" component="div">
-                        EUR: {EUR}€
-                    </Typography>
-                    <Typography className="todayCurrency" variant="h7" component="div">
-                        GBP: {GBP}£
-                    </Typography>
-                    <Typography  variant="h6" component="div">
-                        {date?.replace(/T.*/, '')}
-                    </Typography>
-                </Toolbar>
-            </AppBar>
-        </Box>
-    );
+export default function Header({ date, USD, EUR, GBP }) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar className="color" position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Currency Converter
+          </Typography>
+          <Typography className="todayCurrency" variant="h7" component="div">
+            USD: {USD}$
+          </Typography>
+          <Typography className="todayCurrency" variant="h7" component="div">
+            EUR: {EUR}€
+          </Typography>
+          <Typography className="todayCurrency" variant="h7" component="div">
+            GBP: {GBP}£
+          </Typography>
+          <Typography variant="h6" component="div">
+            {date?.replace(/T.*/, "")}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
